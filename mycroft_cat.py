73a600e31e64316b7ae05b019410dac75609e50b
pazoff <pazoff@gmail.com>
2023-12-18 10:29:42 +0200

    Code reformat

--- mycroft_cat.py
+++ mycroft_cat.py
@@ -8,18 +8,19 @@ try:
 except ImportError as err:
     print(f"Error importing Mycroft Mimic3 TTS: {err}")
     print("Attempting to install Mycroft Mimic3 TTS...")
-   
+
     try:
         subprocess.run(['pip', 'install', 'mycroft-mimic3-tts'], check=True)
         print("Mycroft Mimic3 TTS has been successfully installed.")
     except subprocess.CalledProcessError as e:
         print(f"Error installing Mycroft Mimic3 TTS: {e}")
 
+
 @hook
 def before_cat_sends_message(final_output, cat):
     # Get the current date and time
     current_datetime = datetime.now()
-    
+
     # Format the date and time to use as part of the filename
     formatted_datetime = current_datetime.strftime("%Y%m%d_%H%M%S")
 
@@ -35,7 +36,7 @@ def before_cat_sends_message(final_output, cat):
 
     # Get the message sent by LLM    
     message = final_output["content"]
-    
+
     # Specify the Mimic3 command
     voice = "en_US/ljspeech_low"
     command = ["mimic3", "--cuda", "--voice", voice, message + "."]
@@ -46,7 +47,7 @@ def before_cat_sends_message(final_output, cat):
         mimic3_process = subprocess.run(command, stderr=subprocess.DEVNULL, stdout=output_file.fileno())
 
     mycroft_audio_player = "<audio controls autoplay><source src='" + output_filename + "' type='audio/wav'>Your browser does not support the audio tag.</audio>"
-    
+
     # Send mycroft_audio_player to the user
     cat.send_ws_message(content=mycroft_audio_player, msg_type='chat')
 
