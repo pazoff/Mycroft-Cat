0ba9372024168bca42b0e1ab0e566b4c45f3f868
pazoff <pazoff@gmail.com>
2023-12-19 23:43:25 +0200

    Added option to select diffrent voice speakers and languages English, Italian and Russian

--- mycroft_cat.py
+++ mycroft_cat.py
@@ -1,5 +1,7 @@
 import os
-from cat.mad_hatter.decorators import hook
+from cat.mad_hatter.decorators import hook, plugin
+from pydantic import BaseModel
+from enum import Enum
 import subprocess
 from datetime import datetime
 from threading import Thread
@@ -11,10 +13,35 @@ except ImportError as err:
     print("Attempting to install Mycroft Mimic3 TTS...")
 
     try:
-        subprocess.run(['pip', 'install', 'mycroft-mimic3-tts'], check=True)
+        subprocess.run(['pip', 'install', 'mycroft-mimic3-tts[all]'], check=True)
         print("Mycroft Mimic3 TTS has been successfully installed.")
     except subprocess.CalledProcessError as e:
         print(f"Error installing Mycroft Mimic3 TTS: {e}")
+
+
+# Settings
+
+# Select box
+class VoiceSelect(Enum):
+    Alice: str = 'Alice'
+    Eve: str = 'Eve'
+    Emily: str = 'Emily'
+    Daniel: str = 'Daniel'
+    Dave: str = 'Dave'
+    Angelina: str = 'Angelina'
+    Riccardo: str = 'Riccardo'
+    Nikolaev: str = 'Nikolaev'
+
+
+class MycroftCatSettings(BaseModel):
+    # Select
+    Voice: VoiceSelect = VoiceSelect.Alice
+
+
+# Give your settings schema to the Cat.
+@plugin
+def settings_schema():
+    return MycroftCatSettings.schema()
 
 
 # Function to run Mimic3 process in the background
@@ -29,6 +56,44 @@ def run_mimic3_process(command, output_filename, cat):
     # Generate the audio player HTML and send it as a chat message
     mycroft_audio_player = "<audio controls autoplay><source src='" + output_filename + "' type='audio/wav'>Your browser does not support the audio tag.</audio>"
     cat.send_ws_message(content=mycroft_audio_player, msg_type='chat')
+
+
+# Build Mimic3 command based on selected voice
+def build_mimic_command(llm_message: str, cat):
+    mimic_cmd = ["mimic3", "--cuda"]
+
+    # Load the settings
+    settings = cat.mad_hatter.get_plugin().load_settings()
+    selected_voice = settings.get("Voice")
+
+    # Check if selected_voice is None or not in the specified list
+    if selected_voice not in ["Alice", "Nikolaev", "Daniel", "Angelina", "Emily", "Dave", "Eve", "Riccardo"]:
+        selected_voice = "Alice"
+
+    # Voice mapping dictionary
+    voice_mapping = {
+        "Alice": ("en_US/vctk_low", "p303"),
+        "Eve": ("en_US/ljspeech_low", None),
+        "Daniel": ("en_US/hifi-tts_low", "6097"),
+        "Emily": ("en_US/hifi-tts_low", "92"),
+        "Angelina": ("it_IT/mls_low", "8384"),
+        "Dave": ("en_US/cmu-arctic_low", "aew"),
+        "Nikolaev": ("ru_RU/multi_low", "nikolaev"),
+        "Riccardo": ("it_IT/riccardo-fasol_low", None),
+    }
+
+    # Set default values if selected_voice is not in the mapping
+    voice_cmd, speaker_cmd = voice_mapping.get(selected_voice, ("en_US/vctk_low", "p303"))
+
+    mimic_cmd.extend(["--voice", voice_cmd])
+
+    # Add speaker command if available
+    if speaker_cmd is not None:
+        mimic_cmd.extend(["--speaker", speaker_cmd])
+
+    mimic_cmd.append(llm_message)
+
+    return mimic_cmd
 
 
 # Hook function that runs before sending a message
@@ -52,12 +117,11 @@ def before_cat_sends_message(final_output, cat):
     message = final_output["content"]
 
     # Specify the Mimic3 command
-    voice = "en_US/ljspeech_low"
-    command = ["mimic3", "--cuda", "--voice", voice, message + "."]
+    command = build_mimic_command(message, cat)
 
     # Run the run_mimic3_process function in a separate thread
     mimic3_thread = Thread(target=run_mimic3_process, args=(command, output_filename, cat))
     mimic3_thread.start()
 
-    # Return the final output
+    # Return the final output text, leaving Mimic3 to build the audio file in the background
     return final_output
